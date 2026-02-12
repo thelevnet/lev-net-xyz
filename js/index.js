8775a9c7ae3964283432e17c51901cfb4f4e8a83
thelevnet <the.levnet@gmail.com>
2026-02-12 16:52:39 +0100

    new UI

--- js/index.js
+++ js/index.js
@@ -5,12 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
     tabs.forEach(tab => {
         tab.addEventListener('click', () => {
             const target = tab.getAttribute('data-target');
-
-            // Убираем активный класс у всех табов и панелей
+            
             tabs.forEach(t => t.classList.remove('active'));
             panes.forEach(p => p.classList.remove('active'));
 
-            // Добавляем активный класс текущему табу и нужной панели
             tab.classList.add('active');
             document.getElementById(target).classList.add('active');
         });

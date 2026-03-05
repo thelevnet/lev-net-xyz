5986049ae62fbfaed3de03c6875a66fa77642c21
levnet <the.levnet@gmail.com>
2026-03-05 20:24:24 +0100

    manifest.json

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'lev-net-v1';
+const URLS = [
+  '/',
+  '/chat/',
+  '/manifest.json',
+  '/logo.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
